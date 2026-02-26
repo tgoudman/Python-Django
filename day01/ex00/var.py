588dff68dc19c3d4044978d587afcd299f658386
tgoudman <goudmand76@gmail.com>
2026-02-26 19:08:50 +0100

    introduce day01 python

--- day01/ex00/var.py
+++ day01/ex00/var.py
@@ -1,1 +1,17 @@
-print("Hello world !")
+var = 42
+print(var, " est de type ", type(var))
+var = "42"
+print(var, " est de type ", type(var))
+var = "quarante-deux"
+print(var, " est de type ", type(var))
+var = 42.0
+print(var, " est de type ", type(var))
+var = [1, 2, 3]
+print(var, " est de type ", type(var))
+var = {"42":"42"}
+print(var, " est de type ", type(var))
+var = ("42,", "")
+print(var, " est de type ", type(var))
+var = set()
+print(var, " est de type ", type(var))
+
