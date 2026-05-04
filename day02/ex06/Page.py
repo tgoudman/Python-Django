6b1a7eb15348d81c4c8c80e8257093bb264b9155
tgoudman <goudmand76@gmail.com>
2026-05-04 23:01:53 +0000

    correct ex05

---
+++ day02/ex06/Page.py
@@ -0,0 +1,7 @@
+from elements import Html, Head, Body, Title, Meta, Img, Table, Th, Tr, Td, Ul, Ol, Li, H1, H2, P, Div, Span, Hr, Br, Elem, Text
+
+class Page():
+    def __init__(self):
+        pass
+    def __str__(self):
+        pass
