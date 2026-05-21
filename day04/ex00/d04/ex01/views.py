fdbc734df0c68619ecd48118bc70fbb8bc60c3a6
tgoudman <tgoudman@Debian.myguest.virtualbox.org>
2026-05-21 21:02:50 +0200

    add ex01

--- day04/ex00/d04/ex01/views.py
+++ day04/ex00/d04/ex01/views.py
@@ -2,19 +2,28 @@ import os
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import HttpRequest
+from django.template.exceptions import TemplateDoesNotExist
 
 # Create your views here.
 def ex01Reslver(request: HttpRequest) -> HttpResponse:
     file = f"ex01/{request.path.split('/')[2]}.html"
-    print("DBUG: " + file)
     if file.find("..") != -1:
         response = render(request, "error.html", {"code": "403", "message": "forbidden"})
         response.status_code = 403
         return response
     try:
-        response = render(request, file)
-    except:
-        response = render(request, "error.html", {"code": "404", "message": "file not found"})
-        response.status_code = 404
+        context = {
+            'fruits' : ['Apple', 'Banana', 'Cherry'],
+            'show_message': True,
+            'page_title' : 'Engine of template Django',
+        }
+        response = render(request, file, context)
+    except TemplateDoesNotExist:
+            response = render(request, "error.html", {"code": "404", "message": "file not found"})
+            response.status_code = 404
+    except Exception as e:
+            response = render(request, "error.html", {"code": "500", "message": e})
+            response.status_code = 500
+
 
     return response
